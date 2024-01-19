15654f838866be1422545f150c95ee34c50e9524
todiane Corri <dcorriette@gmail.com>
2024-01-19 17:06:12 +0000

    Update wsgi.py file

--- todianedev/wsgi.py
+++ todianedev/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todianedev.settings')
 
+# Get the Django application
 application = get_wsgi_application()
+
+# Wrap the Django application with Whitenoise
+application = WhiteNoise(application)
+
+
+
